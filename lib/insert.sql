d519175ef4958bc60921bd50f907d529e6aa3911
Nate Miller <natekmiller96@gmail.com>
2018-07-26 18:11:25 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,7 +4,7 @@ INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "low fantasy");
 
 INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "J.K Rowling");
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "Sorcerer's Stone", 2002, 2), (5, "Chamber of Secrets", 2003, 2), (6, "Prisoner of Azkaban", 2005, 2);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
